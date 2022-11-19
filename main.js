ca3f299accf99b5323045f62b4657103cdde65cc
PaulSantiagoValdivia <116080469+PaulSantiagoValdivia@users.noreply.github.com>
2022-11-19 16:16:46 -0300

    HTML index.html shoppingCart. CSS Aside, product detal, shopping cart, JS menuShoppingCart

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
-const menuHamIcon = document.querySelector('.menu')
-const mobileMenu = document.querySelector('.mobile-menu')
+const menuHamIcon = document.querySelector('.menu');
+const menuShoppingCart = document.querySelector('.navbar-shopping-cart');
+const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
+
 menuEmail.addEventListener('click', () => {
     desktopMenu.classList.toggle('inactive');
 });
 menuHamIcon.addEventListener('click', () => {
+    const isAsideClosed = aside.classList.contains('inactive');
+    if(!isAsideClosed){
+        aside.classList.add('inactive')
+    }
     mobileMenu.classList.toggle('inactive');
+});
+menuShoppingCart.addEventListener('click', () => {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    if(!isMobileMenuClosed){
+        mobileMenu.classList.add('inactive');
+    }
+    aside.classList.toggle('inactive');
 });
