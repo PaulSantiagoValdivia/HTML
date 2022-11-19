7bb248becffd98d97d4e8a0df0ccddc1813a1770
PaulSantiagoValdivia <116080469+PaulSantiagoValdivia@users.noreply.github.com>
2022-11-19 15:27:02 -0300

    HTML index.html mobilemenu. CSS mobile-menu. JS mobileMenu.toglle

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const menuEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu')
-
+const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu')
+const mobileMenu = document.querySelector('.mobile-menu')
 menuEmail.addEventListener('click', () => {
-desktopMenu.classList.toggle('inactive');
+    desktopMenu.classList.toggle('inactive');
+});
+menuHamIcon.addEventListener('click', () => {
+    mobileMenu.classList.toggle('inactive');
 });
